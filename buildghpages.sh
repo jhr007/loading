7829b23051d6d7387510bdb052a9ca43122ff2cf
John Robinson <john.holland.robinson@gmail.com>
2015-08-12 19:08:31 -0500

    Save init gh-pages branch cmd

--- buildghpages.sh
+++ buildghpages.sh
@@ -1,3 +1,4 @@
+#git checkout --orphan gh-pages && rm -rf `ls -a | grep -vE '\.gitignore|\.git|node_modules|bower_components|(^[.]{1,2}$)'` && git add -u && git commit -m "initial gh-pages commit"
 echo DIRTY HACK IS BAD
 ember github-pages:commit --environment=gh-pages
 git push origin gh-pages
